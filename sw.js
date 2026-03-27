900de3516df58ee17cf3fae9ffae22a3276ca1cf
Bubber <137005311+100101010100010@users.noreply.github.com>
2026-03-27 19:33:41 -0400

    Add service worker install and activate event listeners

--- sw.js
+++ sw.js
@@ -1,4 +1,10 @@
+self.addEventListener('install', () => {
+    self.skipWaiting();
+});
 
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
 const ADBLOCK = {
     blocked: [
   "googlevideo.com/videoplayback",
